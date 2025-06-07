9ac7c7e4d9a0ea253de24db401ad0167ed8d09cb
Senuka Chandunu <senukaacc@gmail.com>
2025-06-07 18:15:07 +0530

    create: ChatPrototype app

---
+++ ChatPrototype/ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Senuka Chandunu on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
