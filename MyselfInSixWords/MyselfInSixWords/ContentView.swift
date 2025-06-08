9616cf653c22d9a71af3521606042ec6f616bb48
Senuka Chandunu <senukaacc@gmail.com>
2025-06-08 23:47:31 +0530

    update: MyselfInSixWords

--- MyselfInSixWords/MyselfInSixWords/ContentView.swift
+++ MyselfInSixWords/MyselfInSixWords/ContentView.swift
@@ -10,12 +10,42 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Red")
+                .padding()
+                .background(Color.red)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            
+            Text("Blue")
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            
+            Text("Green")
+                .padding()
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            
+            Text("Yellow")
+                .padding()
+                .background(Color.orange)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+
+            Text("Purple")
+                .padding()
+                .background(Color.purple)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+
+            Text("Gray")
+                .padding()
+                .background(Color.gray)
+                .foregroundColor(.white)
+                .cornerRadius(10)
         }
-        .padding()
     }
 }
 
