1e7e1b7c26e6018bad52f648209c1c1f72b4bfb2
Senuka Chandunu <senukaacc@gmail.com>
2025-06-09 22:12:29 +0530

    feat: Prototype a forecast view

--- WeatherForecast/WeatherForecast/ContentView.swift
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -7,13 +7,30 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct WeatherCard: View {
+    let day: String
+    let icon: String
+    let iconColor: Color
+    let high: String
+    let low: String
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Text(day)
+            Image(systemName: icon)
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(iconColor)
+            Text("High: \(high)")
+            Text("Low: \(low)")
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            WeatherCard(day: "Mon", icon: "sun.max.fill", iconColor: .yellow, high: "70°", low: "50°")
+            WeatherCard(day: "Tue", icon: "cloud.rain.fill", iconColor: .blue, high: "60°", low: "40°")
         }
         .padding()
     }
