68f371451d26e7785623471d69ca10a582c43344
Senuka Chandunu <senukaacc@gmail.com>
2025-06-11 22:40:40 +0530

    @State property wrapper & withAnimation

--- DiceRoller/DiceRoller/DiceView.swift
+++ DiceRoller/DiceRoller/DiceView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct DiceView: View {
+    @State private var numberOfPips: Int = 1
+    
     var body: some View {
-        Image(systemName: "die.face.1")
+        VStack {
+            Image(systemName: "die.face.\(numberOfPips)")
+                .resizable()
+                .frame(width: 100, height: 100)
+            
+            Button("Roll") {
+                withAnimation{
+                    numberOfPips = Int.random(in: 1...6)
+                }
+            }
+                .buttonStyle(.bordered)
+            }
+        }
     }
-}
+    
+    #Preview {
+        DiceView()
+    }
 
-#Preview {
-    DiceView()
-}
