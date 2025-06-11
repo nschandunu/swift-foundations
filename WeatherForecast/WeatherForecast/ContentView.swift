44d06eeac47c19306b767a04d413cbb9b078a74b
Senuka Chandunu <senukaacc@gmail.com>
2025-06-11 15:02:30 +0530

    Fix DayForecast component: rename, dynamic temps, casing fix, styling

--- WeatherForecast/WeatherForecast/ContentView.swift
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -11,9 +11,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            DayForcast(day: "Mon" , high: 70 , low: 50)
-            DayForcast(day: "Tue" , high: 80 , low: 40)
-            DayForcast(day: "Wed" , high: 60 , low: 55)
+            DayForcast(day: "Mon" , high: 70 , low: 50 , isRainy: true)
+            DayForcast(day: "Tue" , high: 80 , low: 40 , isRainy: false)
+            DayForcast(day: "Wed" , high: 60 , low: 55 , isRainy: false)
         }
     }
 }
@@ -26,15 +26,38 @@ struct ContentView: View {
 struct DayForcast: View {
     let day: String
     let high,low: Int
-    var body: some View {
-        VStack {
-            Text(day)
-            Image(systemName: "sun.max.fill")
-                .imageScale(.large)
-                .foregroundStyle(.yellow)
-            Text("High: \(high)°")
-            Text("Low: \(low)°")
+    let isRainy: Bool
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
         }
-        .padding()
     }
-}
+        
+    var iconColor: Color {
+        if isRainy {
+            return .blue
+        } else {
+            return .yellow
+        }
+    }
+        
+        var body: some View {
+            VStack {
+                Text(day)
+                    .font(.headline)
+                Image(systemName: iconName)
+                    .foregroundStyle(iconColor)
+                    .font(Font.largeTitle)
+                    .padding(5)
+                Text("High: \(high)°")
+                    .fontWeight(Font.Weight.semibold)
+                Text("Low: \(low)°")
+                    .fontWeight(Font.Weight.medium)
+                    .foregroundStyle(Color.secondary)
+            }
+            .padding()
+        }
+    }
