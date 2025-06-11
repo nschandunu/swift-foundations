288b4bbd85743bea26906598b8fa418adedc8f8b
Senuka Chandunu <senukaacc@gmail.com>
2025-06-11 16:14:06 +0530

    create: FeatureCard style

---
+++ OnboardingFlow/OnboardingFlow/FeatureCard.swift
@@ -0,0 +1,30 @@
+//
+//  FeatureCard.swift
+//  OnboardingFlow
+//
+//  Created by Senuka Chandunu on 6/11/25.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    let iconName: String
+    let description: String
+    
+    var body: some View {
+        HStack{
+            Image(systemName: iconName)
+                .font(.largeTitle)
+            
+            Text(description)
+        }
+        .padding()
+        .background(.tint, in: RoundedRectangle(cornerRadius: 12))
+        .foregroundStyle(.white)
+    }
+}
+
+#Preview {
+    FeatureCard(iconName: "person.2.crop.square.stack.fill",
+                description: "A multiline description about a feature paired with the image on the left.")
+}
--- OnboardingFlow/OnboardingFlow/FeaturesPage.swift
+++ OnboardingFlow/OnboardingFlow/FeaturesPage.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct FeaturesPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Features")
+                .font(.title)
+                .fontWeight(.semibold)
+                .padding(.bottom)
+            
+            FeatureCard(iconName: "person.2.crop.square.stack.fill",
+                                    description: "A multiline description about a feature paired with the image on the left.")
+            
+            FeatureCard(iconName: "quote.bubble.fill", description: "Short summary")
+        }
+        .padding()
     }
 }
 
