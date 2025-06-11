b4049660837a451951b80563746a83dc30165b73
Senuka Chandunu <senukaacc@gmail.com>
2025-06-11 23:02:08 +0530

    feat: Dice HStack & withAnimation

--- DiceRoller/DiceRoller/ContentView.swift
+++ DiceRoller/DiceRoller/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numberOfDice: Int = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dice Roller")
+                .font(.largeTitle.lowercaseSmallCaps())
+                
+            HStack {
+                ForEach(1...numberOfDice, id: \.description) { _ in
+                    DiceView()
+                }
+            }
+            HStack {
+                Button("Remove") {
+                    withAnimation {
+                        numberOfDice -= 1
+                    }
+                }
+                .disabled(numberOfDice == 1)
+                            
+                Button("Add") {
+                    withAnimation {
+                        numberOfDice += 1
+                    }
+                }
+                .disabled(numberOfDice == 3)
+            }
+                .padding()
         }
         .padding()
+        
     }
 }
 
