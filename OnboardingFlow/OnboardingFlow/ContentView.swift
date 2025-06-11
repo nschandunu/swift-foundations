26a6c96e845e7e4cdc40e39d8bd8c6612276151c
Senuka Chandunu <senukaacc@gmail.com>
2025-06-11 16:32:00 +0530

    gradient colors to ContentView

--- OnboardingFlow/OnboardingFlow/ContentView.swift
+++ OnboardingFlow/OnboardingFlow/ContentView.swift
@@ -7,12 +7,18 @@
 
 import SwiftUI
 
+let gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
 struct ContentView: View {
     var body: some View {
         TabView{
             WelcomePage()
             FeaturesPage()
         }
+        .background(Gradient(colors: gradientColors))
         .tabViewStyle(.page)
     }
 }
