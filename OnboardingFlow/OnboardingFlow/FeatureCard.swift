dddfb79fa293f12e38b6a1c15a67e4d81870875e
Senuka Chandunu <senukaacc@gmail.com>
2025-06-11 16:16:42 +0530

    feat: Spacer for max width

--- OnboardingFlow/OnboardingFlow/FeatureCard.swift
+++ OnboardingFlow/OnboardingFlow/FeatureCard.swift
@@ -15,8 +15,12 @@ struct FeatureCard: View {
         HStack{
             Image(systemName: iconName)
                 .font(.largeTitle)
+                .frame(width: 50)
+                .padding(.trailing, 10)
             
             Text(description)
+            
+            Spacer()
         }
         .padding()
         .background(.tint, in: RoundedRectangle(cornerRadius: 12))
