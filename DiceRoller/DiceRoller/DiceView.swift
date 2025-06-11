600d91cce30cd29836a7eab78f3ba53dcd481dbd
Senuka Chandunu <senukaacc@gmail.com>
2025-06-11 22:23:49 +0530

    create: DiceRoller app

---
+++ DiceRoller/DiceRoller/DiceView.swift
@@ -0,0 +1,18 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by Senuka Chandunu on 6/11/25.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Image(systemName: "die.face.1")
+    }
+}
+
+#Preview {
+    DiceView()
+}
