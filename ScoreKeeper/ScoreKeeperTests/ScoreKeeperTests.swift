664d4efd71335b383e6855d63a635e76df157ea2
Senuka Chandunu <senukaacc@gmail.com>
2025-06-14 15:13:49 +0530

    iOS Unit testing bundle

---
+++ ScoreKeeper/ScoreKeeperTests/ScoreKeeperTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScoreKeeperTests.swift
+//  ScoreKeeperTests
+//
+//  Created by Senuka Chandunu on 6/14/25.
+//
+
+import Testing
+
+struct ScoreKeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
