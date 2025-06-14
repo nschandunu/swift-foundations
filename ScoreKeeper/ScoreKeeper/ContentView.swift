91bf7ad4150cf93b517e6fc508a436f5bc5d4f7f
Senuka Chandunu <senukaacc@gmail.com>
2025-06-14 14:26:32 +0530

    fix: preview crash

--- ScoreKeeper/ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ScoreKeeper/ContentView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var players: [String] = ["Saman", "Kamal", "Nimal"]
+    @State private var scores: [Int] = [0,0,0]
     
     var body: some View {
         VStack {
             ForEach(0..<players.count, id: \.description) { index in
                 TextField("Name", text: $players[index])
+                Stepper("\(scores[index])", value: $scores[index])
             }
             Button("Add Player", systemImage: "plus") {
                 players.append("")
