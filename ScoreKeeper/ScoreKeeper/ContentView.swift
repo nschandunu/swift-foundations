60d3efdc9de0e6cb2f12967195f53689aaaf705e
Senuka Chandunu <senukaacc@gmail.com>
2025-06-14 14:37:38 +0530

    Improve app design

--- ScoreKeeper/ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ScoreKeeper/ContentView.swift
@@ -8,18 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var players: [String] = ["Saman", "Kamal", "Nimal"]
-    @State private var scores: [Int] = [0,0,0]
+    @State private var players: [Player] = [
+        Player(name: "Elisha", score: 0),
+        Player(name: "Andre", score: 0),
+        Player(name: "Jasmine", score: 0),
+    ]
     
     var body: some View {
-        VStack {
-            ForEach(0..<players.count, id: \.description) { index in
-                TextField("Name", text: $players[index])
-                Stepper("\(scores[index])", value: $scores[index])
+        VStack(alignment: .leading) {
+            Text("Score Keeper")
+                .font(.title)
+                .bold()
+                .padding(.bottom)
+            
+            Grid{
+                GridRow {
+                    Text("Player")
+                        .gridColumnAlignment(.leading)
+                    Text("Score")
+                }
+                .font(.headline)
+                
+                ForEach($players) { $player in
+                    GridRow {
+                        TextField("Name", text: $player.name)
+                        Text("\(player.score)")
+                        Stepper("\(player.score)", value: $player.score)
+                            .labelsHidden()
+                            
+                    }
+                }
             }
+            .padding(.vertical)
             Button("Add Player", systemImage: "plus") {
-                players.append("")
-                scores.append(0)
+                players.append(Player(name: "", score: 0))
             }
             Spacer()
         }
