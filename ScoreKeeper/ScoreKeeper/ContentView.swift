d6c94290de51dd219a7dee4c5b4936f740bae4d3
Senuka Chandunu <senukaacc@gmail.com>
2025-06-14 23:19:29 +0530

    Improve app UI design

--- ScoreKeeper/ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ScoreKeeper/ContentView.swift
@@ -19,12 +19,14 @@ struct ContentView: View {
                 .padding(.bottom)
             
             SettingsView(doesHighestScoreWin: $scoreboard.doesHighestScoreWin, startingPoints: $startingPoints)
+                .disabled(scoreboard.state != .setup)
             
             Grid{
                 GridRow {
                     Text("Player")
                         .gridColumnAlignment(.leading)
                     Text("Score")
+                        .opacity(scoreboard.state == .setup ? 0 : 1.0)
                 }
                 .font(.headline)
                 
@@ -36,10 +38,13 @@ struct ContentView: View {
                                     .foregroundStyle(Color.yellow)
                             }
                             TextField("Name", text: $player.name)
+                                .disabled(scoreboard.state != .setup)
                         }
                         Text("\(player.score)")
+                            .opacity(scoreboard.state == .setup ? 0 : 1.0)
                         Stepper("\(player.score)", value: $player.score)
                             .labelsHidden()
+                            .opacity(scoreboard.state == .setup ? 0 : 1.0)
                             
                     }
                 }
@@ -49,23 +54,32 @@ struct ContentView: View {
             Button("Add Player", systemImage: "plus") {
                 scoreboard.players.append(Player(name: "", score: 0))
             }
+            .opacity(scoreboard.state == .setup ? 1.0 : 0)
             Spacer()
             
-            switch scoreboard.state {
-            case .setup:
-                Button("Start Game", systemImage: "play.fill") {
-                    scoreboard.state = .playing
-                    scoreboard.resetScores(to: startingPoints)
+            HStack {
+                Spacer()
+                switch scoreboard.state {
+                case .setup:
+                    Button("Start Game", systemImage: "play.fill") {
+                        scoreboard.state = .playing
+                        scoreboard.resetScores(to: startingPoints)
+                    }
+                case .playing:
+                    Button("End Game", systemImage: "stop.fill") {
+                        scoreboard.state = .gameOver
+                    }
+                case .gameOver:
+                    Button("Reset Game", systemImage: "arrow.counterclockwise") {
+                        scoreboard.state = .setup
+                    }
                 }
-            case .playing:
-                Button("End Game", systemImage: "stop.fill") {
-                    scoreboard.state = .gameOver
-                }
-            case .gameOver:
-                Button("Reset Game", systemImage: "arrow.counterclockwise") {
-                    scoreboard.state = .setup
-                }
+                Spacer()
             }
+            .buttonStyle(.bordered)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            .tint(.blue)
         }
         .padding()
     }
