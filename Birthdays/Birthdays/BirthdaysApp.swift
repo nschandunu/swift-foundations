902425bf36cca2a12f1a2de351c3a1912807a21e
Senuka Chandunu <senukaacc@gmail.com>
2025-06-15 09:38:16 +0530

    create: birthdays app

---
+++ Birthdays/Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Senuka Chandunu on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
