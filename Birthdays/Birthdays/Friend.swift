a1b4a464607f03a521404dcd227f5a0b4e1e8576
Senuka Chandunu <senukaacc@gmail.com>
2025-06-16 22:37:46 +0530

    SwiftData model

--- Birthdays/Birthdays/Friend.swift
+++ Birthdays/Birthdays/Friend.swift
@@ -6,8 +6,16 @@
 //
 
 import Foundation
+import SwiftData
 
-struct Friend {
+@Model
+class Friend {
     var name: String
     var birthday: Date
+    
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
 }
