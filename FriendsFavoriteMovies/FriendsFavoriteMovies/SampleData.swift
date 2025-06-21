ee148b11faba1123cf2041aa2ac063ffb4d1e954
Senuka Chandunu <senukaacc@gmail.com>
2025-06-21 19:07:20 +0530

    fix : Sample Data issue

--- FriendsFavoriteMovies/FriendsFavoriteMovies/SampleData.swift
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/SampleData.swift
@@ -12,34 +12,28 @@ import SwiftData
 @MainActor
 class SampleData {
     static let shared = SampleData()
-    
-    
+
+
     let modelContainer: ModelContainer
-    
-    
+
+
     var context: ModelContext {
         modelContainer.mainContext
     }
-    
-    
+
+
     private init() {
         let schema = Schema([
             Friend.self,
             Movie.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
-        
-        
+
+
         do {
-            modelContainer = try ModelContainer(for: schema, configurations: insertSampleData()
-                try context.save()
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
-        }
-    }
-    private func insertSampleData() {
-        for friend in Friend.sampleData {
-            context.insert(friend)
         }
     }
 }
